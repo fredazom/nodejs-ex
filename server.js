bdb472fa0ea9532ecdf21a71ca7c60db1e798387
fredy <fredazom@gmail.com>
2017-09-30 18:12:26 +0200

    add test route to check process.env

--- server.js
+++ server.js
@@ -89,6 +89,11 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/test', function (req, res) {
+  var a = (typeof process.env.DATABASE_SERVICE_NAME != 'undefined')?process.env.DATABASE_SERVICE_NAME:'no process env';
+  res.send(a);
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
